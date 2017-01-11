fe9c7f49cd68e2b7d7ca622f4096cc22f2838a0b
Roy Liu <liup1@qq.com>
2017-01-11 13:38:37 +0800

    增加生成超链接函数write_url
    增加生成超链接函数write_url

--- XLSWrite.py
+++ XLSWrite.py
@@ -15,5 +15,12 @@ class XLSWrite():
             print(x,y,value)
             print(e)
 
+    def write_url(self, x, y, value, name):
+    try:
+        self.sheet.write(x, y, Formula('HYPERLINK("' + value + '";"' + name + '")'))
+    except Exception as e:
+        print(x, y, value, name)
+        print(e)
+
     def save(self, filepath):
         self.xls.save(filepath)
